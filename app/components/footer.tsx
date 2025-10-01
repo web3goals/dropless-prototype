4dd341e46b55be5c396e90d8d83a583ca1f1c54e
kiv1n <aloha@kiv1n.ru>
2025-10-01 14:56:40 +0300

    Refactor Footer component for improved button layout and styling

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,28 +1,40 @@
 "use client";
 
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
-import Link from "next/link";
 
 export function Footer() {
   const pathname = usePathname();
 
   return (
     <footer className="sticky bottom-0 z-30 bg-background border-t">
-      <div className="container mx-auto flex flex-row gap-2 items-center justify-center h-16 px-4">
+      <div className="container mx-auto flex flex-row gap-2 items-center justify-center py-4 px-4">
         <Link href="/household">
-          <Button variant={pathname === "/household" ? "default" : "secondary"}>
-            🏠 Household
+          <Button
+            variant={pathname === "/household" ? "default" : "secondary"}
+            className="flex flex-col gap-1 h-auto w-28"
+          >
+            <p>🏠</p>
+            <p>Household</p>
           </Button>
         </Link>
         <Link href="/challenge">
-          <Button variant={pathname === "/challenge" ? "default" : "secondary"}>
-            🏆 Challenge
+          <Button
+            variant={pathname === "/challenge" ? "default" : "secondary"}
+            className="flex flex-col gap-1 h-auto w-28"
+          >
+            <p>🏆</p>
+            <p>Challenge</p>
           </Button>
         </Link>
         <Link href="/faq">
-          <Button variant={pathname === "/faq" ? "default" : "secondary"}>
-            ❓ FAQ
+          <Button
+            variant={pathname === "/faq" ? "default" : "secondary"}
+            className="flex flex-col gap-1 h-auto w-28"
+          >
+            <p>❓</p>
+            <p>FAQ</p>
           </Button>
         </Link>
       </div>
